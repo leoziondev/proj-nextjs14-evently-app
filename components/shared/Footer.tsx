776b840f5ae1642f15fc772a35d6452186cdea56
leoziondev <leozionsuporte@gmail.com>
2023-12-16 11:08:24 -0300

    style footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Logo Image"
+          />
+        </Link>
+        <p>2023 Evently. All rights reserverd.</p>
+      </div>
+    </footer>
   )
 }
