d965f48f33b002266e3221c688a2ec0083ffa5f3
leoziondev <leozionsuporte@gmail.com>
2023-12-16 01:29:25 -0300

    config clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
 import { Poppins } from 'next/font/google'
+
 import './globals.css'
 
 const poppins = Poppins({ 
@@ -22,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
