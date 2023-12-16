d58bb965a3cf66eb90dc275f8b6126fdee80745d
leoziondev <leozionsuporte@gmail.com>
2023-12-16 00:40:37 -0300

    add logo header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className="w-full border-b">
+        <div className="wrapper flex items-center justify-between">
+            <Link href="/" className="w-36">
+                <Image
+                    src="/assets/images/logo.svg"
+                    width={128}
+                    height={38}
+                    alt="Evently logo"
+                />
+            </Link>
+
+            <div className="flex w-32 justify-end gap-3">
+                
+            </div>
+        </div>
+    </div>
   )
 }
